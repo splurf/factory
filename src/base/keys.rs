adb2b069b2c93fb8342e29be894ece3f4cf4a993
splurf <evandotschwartzentruber@gmail.com>
2024-05-28 12:50:14 -0400

    impl `Entry` wrapper for flagging system

--- src/base/keys.rs
+++ src/base/keys.rs
@@ -1,10 +1,45 @@
 use serenity::prelude::TypeMapKey;
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Deref};
 
-use crate::Item;
+use crate::Event;
 
-pub struct Items;
+pub struct Entry {
+    inner: Event,
+    flagged: bool,
+}
 
-impl TypeMapKey for Items {
-    type Value = HashMap<String, Item>;
+impl Entry {
+    pub const fn new(inner: Event) -> Self {
+        Self {
+            inner,
+            flagged: false,
+        }
+    }
+
+    pub fn update(&mut self, inner: Event) {
+        self.inner = inner;
+        self.flagged = false
+    }
+
+    pub const fn is_flagged(&self) -> bool {
+        self.flagged
+    }
+
+    pub fn flag(&mut self) {
+        self.flagged = true
+    }
+}
+
+impl Deref for Entry {
+    type Target = Event;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+pub struct Events;
+
+impl TypeMapKey for Events {
+    type Value = HashMap<String, Entry>;
 }
